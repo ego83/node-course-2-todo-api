8f8a88180bf30899a57550bdb65a425fae080402
Eugen Panov <egosign@gmail.com>
2017-05-18 19:27:13 +0200

    add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: "first est to do"
 }, {
   _id: new ObjectId(),
-  text: "first est to do 2"
+  text: "first est to do 2",
+  completed: true,
+  completedAt: 222
 }];
 
 beforeEach((done) => {
@@ -140,4 +142,42 @@ describe('DELETE /todos:id', () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe('PATCH /todos:id', () => {
+  it('should update a todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+
+    var body = {"text": "werlfksjdglkfjgfg", "completed": true};
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send(body)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(body.text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    var body = {"text": "werlfksjdglkfjgfg", "completed": false};
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send(body)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(body.text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+
 });
