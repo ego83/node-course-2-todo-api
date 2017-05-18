4b8e3961e96851ac5849ab303e7332a3c1e1e60c
Eugen Panov <egosign@gmail.com>
2017-05-18 19:43:16 +0200

    setup separate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env +++++++', env);
+
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production') {
+  process.env.MONGODB_URI = 'mongodb://root:pfuAkMY4wRt2@ds133981.mlab.com:33981/node-todo-api';  
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // pfuAkMY4wRt2
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://root:pfuAkMY4wRt2@ds133981.mlab.com:33981/node-todo-api');
 
 module.exports = {mongoose};
