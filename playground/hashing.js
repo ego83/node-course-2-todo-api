34cedcc7d9921247e475f52a7ff79a2c99d1b6b9
Eugen Panov <egosign@gmail.com>
2017-05-20 12:53:30 +0200

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const jwt      = require('jsonwebtoken');
+const bcrypt   = require('bcryptjs');
 
-var data = {
-  id: 9
-}
+var password = "123abc!";
 
-var token = jwt.sign(data, 'asdfgh')
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, 'asdfgh');
-console.log(decoded);
+
+var hashedPassword = "$2a$10$m/8vI4i9DRx.NXGQzm9Reu2vymCNzdtnvo0voMVLS69MldXUssk5m";
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// var data = {
+//   id: 9
+// }
+//
+// var token = jwt.sign(data, 'asdfgh')
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'asdfgh');
+// console.log(decoded);
 
 // var message = 'i am user number 4';
 // var hash = SHA256(message).toString();
